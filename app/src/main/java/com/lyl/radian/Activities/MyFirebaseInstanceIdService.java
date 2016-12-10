0758605165b725452ebc2ed7b7214168b34da653
Len <len13@hotmail.de>
2016-12-10 22:30:55 +0100

    MyFirebaseInstanceIdService

--- app/src/main/java/com/lyl/radian/Activities/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/lyl/radian/Activities/MyFirebaseInstanceIdService.java
@@ -1,26 +1,15 @@
 package com.lyl.radian.Activities;
 
-/**
- * Created by Yannick on 08.12.2016.
- */
-
 import android.util.Log;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
-import com.lyl.radian.Utilities.Constants;
 
 /**
- * Created by Belal on 5/27/2016.
+ * Created by len13 on 10.12.2016.
  */
 
-
-//Class extending FirebaseInstanceIdService
-public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
-
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
     private static final String TAG = "MyFirebaseIIDService";
 
     @Override
